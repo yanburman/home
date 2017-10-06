d072b8058470b9155012640b9bd1283c6c0e2dad
Yan Burman <yanburman@users.noreply.github.com>
2017-10-06 12:27:39 +0300

    Update rename_consecutive.py

--- bin/rename_consecutive.py
+++ bin/rename_consecutive.py
@@ -3,6 +3,8 @@
 import glob
 import sys
 import os
+import argparse
+
 
 def rename(folder, ext):
     curr_idx = 1
@@ -11,7 +13,9 @@ def rename(folder, ext):
         os.rename(f, '{prefix:s}{idx:05d}.{ext:%s}'.format(prefix=prefix, idx=curr_idx, ext=ext))
         curr_idx += 1
 
+
 def main():
     rename(sys.argv[1], sys.argv[2])
+
 
 main()
