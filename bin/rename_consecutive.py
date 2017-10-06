56e35e95d6446c8870c27d0493d3057f9d85393f
Yan Burman <yanburman@users.noreply.github.com>
2017-10-06 12:23:27 +0300

    Create rename_consecutive.py

---
+++ bin/rename_consecutive.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import glob
+import sys
+import os
+
+def rename(folder, ext):
+    curr_idx = 1
+    prefix = 'f_'
+    for f in glob.glob(os.path.join(folder, '*.%s' % ext)):
+        os.rename(f, '{prefix:s}{idx:05d}.{ext:%s}'.format(prefix=prefix, idx=curr_idx, ext=ext))
+        curr_idx += 1
+
+def main():
+    rename(sys.argv[1], sys.argv[2])
+
+main()
